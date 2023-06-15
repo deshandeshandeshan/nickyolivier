387470978d283fdaeb0badd0d90c4ba5c362c883
Deshan Mclachlan <deshan.mclachlan@developersinstitute.co.nz>
2023-06-16 11:20:48 +1200

    Configured header to include logo and routes to other pages

---
+++ src/components/Header.js
@@ -0,0 +1,49 @@
+import { Link } from "react-router-dom";
+import Logo from "../images/NickyOlivierLogoOffWhite.png";
+
+const Header = () => {
+  return (
+    <header>
+      <nav className="navBar">
+        <Link to="/" className="navLink">
+          <img
+            src={Logo}
+            alt="Nicky Olivier logo"
+            width="200"
+            className="navBrand navBrandPadding"
+          />
+        </Link>
+
+        <ul className="navMenu navPadding">
+          <li className="navItem navPadding">
+            <Link to="/" className="navLink">
+              Home
+            </Link>
+          </li>
+          <li className="navItem navPadding">
+            <Link to="/gallery" className="navLink">
+              Gallery
+            </Link>
+          </li>
+          <li className="navItem navPadding">
+            <Link to="/process" className="navLink">
+              Process
+            </Link>
+          </li>
+          <li className="navItem navPadding">
+            <Link to="/contact" className="navLink">
+              Contact
+            </Link>
+          </li>
+          <li className="navItem navPadding">
+            <Link to="/aboutme" className="navLink">
+              About me
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
