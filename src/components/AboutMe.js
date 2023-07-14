d76e33a7c131c9b7ad8f2ea0e83d48ac6dd5e652
Deshan Mclachlan <deshan.mclachlan@developersinstitute.co.nz>
2023-07-14 14:19:06 +1200

    about me page styled for smaller screens

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -6,15 +6,17 @@ import "./AboutMe.css";
 const AboutMe = () => {
   return (
     <main className="about-me">
-      <div className="about-me-intro">
+      <div className="about-me-intro about-me-grid">
         <img
           src={aboutMeIntroImage}
           alt="woman walking through large field"
-          className="about-me-intro-image"
-          width="500"
+          className="about-me-intro-image about-me-section-padding"
+          width="100%"
         />
-        <h2 className="about-me-intro-heading">Hello!</h2>
-        <p className="about-me-intro-text">
+        <h2 className="about-me-intro-heading about-me-text-align about-me-text-padding">
+          Hello!
+        </h2>
+        <p className="about-me-intro-text about-me-text-align">
           Lorem ipsum dolor sit amet consectetur. Auctor semper hac eu
           elementum. Lacus consectetur porttitor pulvinar ac aliquam risus
           dictum scelerisque. Cras enim consequat ut molestie facilisis risus
@@ -22,14 +24,17 @@ const AboutMe = () => {
           arcu faucibus. In ac aliquam at sed in aliquet.
         </p>
       </div>
-      <div className="about-me-main">
+
+      <div className="about-me-section-divider"></div>
+
+      <div className="about-me-main about-me-main-grid about-me-section-padding">
         <img
           src={hairStyling}
           alt="woman getting her hair styled by hairdresser"
-          className="about-me-main-image"
-          width="500"
+          className="about-me-main-image about-me-section-padding"
+          width="100%"
         />
-        <p className="about-me-main-text">
+        <p className="about-me-main-text about-me-section-padding about-me-text-align">
           Lorem ipsum dolor sit amet consectetur. Auctor semper hac eu
           elementum. Lacus consectetur porttitor pulvinar ac aliquam risus
           dictum scelerisque. Cras enim consequat ut molestie facilisis risus
