ce38b63385618ba516b3a1cac38de69bd318d46e
Deshan Mclachlan <deshan.mclachlan@developersinstitute.co.nz>
2023-07-21 14:27:40 +1200

    In imageSlider component, added react buttons package - added buttons to component. Mapped onclick events to buttons and set up logic to go to next and prev images

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./ImageSlider.css";
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from "react-icons/fa";
 
 const ImageSlider = () => {
   const imageSlides = [
@@ -7,17 +8,45 @@ const ImageSlider = () => {
       image: require("../images/wedding-hair-1.jpg"),
       title: "Annies wedding hair",
     },
+    {
+      image: require("../images/wedding-hair-2.jpg"),
+      title: "Annies wedding hair",
+    },
+    {
+      image: require("../images/wedding-hair-3.jpg"),
+      title: "Annies wedding hair",
+    },
+    {
+      image: require("../images/wedding-hair-4.jpg"),
+      title: "Annies wedding hair",
+    },
   ];
 
   const [currentIndex, setCurrentIndex] = useState(0);
 
+  const goToPrev = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? imageSlides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToNext = () => {
+    const isLastSlide = currentIndex === imageSlides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
   return (
-    <div className="image-slide-container">
-      <img
-        className="image-slide"
-        src={`${imageSlides[currentIndex].image}`}
-        alt="woman holding flowers looking out into a lake"
-      />
+    <div>
+      <div className="image-slide-container">
+        <img
+          className="image-slide"
+          src={`${imageSlides[currentIndex].image}`}
+          alt="woman holding flowers looking out into a lake"
+        />
+      </div>
+      <FaArrowAltCircleLeft onClick={goToPrev} />
+      <FaArrowAltCircleRight onClick={goToNext} />
     </div>
   );
 };
