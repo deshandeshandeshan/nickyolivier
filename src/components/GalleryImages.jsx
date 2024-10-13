d3dc9e1eb7cfe01f539a2e9c01a40c036e4982a8
Deshan Mclachlan <deshan.mclachlan0@gmail.com>
2024-10-13 14:04:15 +1100

    Re-created the gallery and added new images

---
+++ src/components/GalleryImages.jsx
@@ -0,0 +1,103 @@
+import weddingHairImage1 from "../finalImages/Gallery (1).webp";
+import weddingHairImage2 from "../finalImages/Gallery (2).webp";
+import weddingHairImage3 from "../finalImages/Gallery (3).webp";
+import weddingHairImage4 from "../finalImages/Gallery (4).webp";
+import weddingHairImage5 from "../finalImages/Gallery (5).webp";
+import weddingHairImage6 from "../finalImages/Gallery (6).webp";
+import weddingHairImage7 from "../finalImages/Gallery (7).webp";
+import weddingHairImage8 from "../finalImages/Gallery (8).webp";
+import weddingHairImage9 from "../finalImages/Gallery (9).webp";
+import weddingHairImage10 from "../finalImages/Gallery (10).webp";
+import weddingHairImage11 from "../finalImages/Gallery (11).webp";
+import weddingHairImage12 from "../finalImages/Gallery (12).webp";
+import weddingHairImage13 from "../finalImages/Gallery (13).webp";
+import weddingHairImage14 from "../finalImages/Gallery (14).webp";
+import weddingHairImage15 from "../finalImages/Gallery (15).webp";
+import weddingHairImage16 from "../finalImages/Gallery (16).webp";
+import weddingHairImage17 from "../finalImages/Gallery (17).webp";
+import weddingHairImage18 from "../finalImages/Gallery (18).webp";
+import weddingHairImage19 from "../finalImages/Gallery (19).webp";
+import weddingHairImage20 from "../finalImages/GalleryNew1.webp";
+import weddingHairImage21 from "../finalImages/GalleryNew2.webp";
+import weddingHairImage22 from "../finalImages/GalleryNew3.webp";
+import weddingHairImage23 from "../finalImages/GalleryNew4.webp";
+import weddingHairImage24 from "../finalImages/GalleryNew5.webp";
+import weddingHairImage25 from "../finalImages/GalleryNew6.webp";
+import weddingHairImage26 from "../finalImages/GalleryNew7.webp";
+import weddingHairImage27 from "../finalImages/GalleryNew8.webp";
+import weddingHairImage28 from "../finalImages/GalleryNew9.webp";
+import weddingHairImage29 from "../finalImages/GalleryNew10.webp";
+import weddingHairImage30 from "../finalImages/GalleryNew11.webp";
+import weddingHairImage31 from "../finalImages/GalleryNew12.webp";
+import weddingHairImage32 from "../finalImages/GalleryNew13.webp";
+import weddingHairImage33 from "../finalImages/GalleryNew14.webp";
+import weddingHairImage34 from "../finalImages/GalleryNew15.webp";
+import weddingHairImage35 from "../finalImages/GalleryNew16.webp";
+import weddingHairImage36 from "../finalImages/GalleryNew17.webp";
+import weddingHairImage37 from "../finalImages/GalleryNew18.webp";
+import weddingHairImage38 from "../finalImages/GalleryNew19.webp";
+import weddingHairImage39 from "../finalImages/GalleryNew20.webp";
+import weddingHairImage40 from "../finalImages/GalleryNew21.webp";
+import weddingHairImage41 from "../finalImages/GalleryNew22.webp";
+import weddingHairImage42 from "../finalImages/GalleryNew23.webp";
+import weddingHairImage43 from "../finalImages/GalleryNew24.webp";
+import weddingHairImage44 from "../finalImages/GalleryNew25.webp";
+import weddingHairImage45 from "../finalImages/GalleryNew26.webp";
+import weddingHairImage46 from "../finalImages/GalleryNew27.webp";
+import weddingHairImage47 from "../finalImages/GalleryNew28.webp";
+import weddingHairImage48 from "../finalImages/GalleryNew29.webp";
+import weddingHairImage49 from "../finalImages/GalleryNew30.webp";
+
+const galleryImages = [
+  weddingHairImage1,
+  weddingHairImage2,
+  weddingHairImage3,
+  weddingHairImage4,
+  weddingHairImage5,
+  weddingHairImage6,
+  weddingHairImage7,
+  weddingHairImage8,
+  weddingHairImage9,
+  weddingHairImage10,
+  weddingHairImage11,
+  weddingHairImage12,
+  weddingHairImage13,
+  weddingHairImage14,
+  weddingHairImage15,
+  weddingHairImage16,
+  weddingHairImage17,
+  weddingHairImage18,
+  weddingHairImage19,
+  weddingHairImage20,
+  weddingHairImage21,
+  weddingHairImage22,
+  weddingHairImage23,
+  weddingHairImage24,
+  weddingHairImage25,
+  weddingHairImage26,
+  weddingHairImage27,
+  weddingHairImage28,
+  weddingHairImage29,
+  weddingHairImage30,
+  weddingHairImage31,
+  weddingHairImage32,
+  weddingHairImage33,
+  weddingHairImage34,
+  weddingHairImage35,
+  weddingHairImage36,
+  weddingHairImage37,
+  weddingHairImage38,
+  weddingHairImage39,
+  weddingHairImage40,
+  weddingHairImage41,
+  weddingHairImage42,
+  weddingHairImage43,
+  weddingHairImage44,
+  weddingHairImage45,
+  weddingHairImage46,
+  weddingHairImage47,
+  weddingHairImage48,
+  weddingHairImage49,
+];
+
+export default galleryImages;
